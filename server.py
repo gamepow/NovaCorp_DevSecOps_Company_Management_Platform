c17a6ef0a29d3be411b74e0866c6a24ea0c226e1
Renato Montenegro <ramontenegropalma@outlook.com>
2026-05-04 20:45:10 +0200

    fix: enforce secure file permissions in docker, implement environment-based secret management, and update session lifetime configuration

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
+
+load_dotenv() # Load environment variables from .env file
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
-app.permanent_session_lifetime = 99999999
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+app.permanent_session_lifetime = timedelta(hours=1)
 
 @app.errorhandler(404)
 def not_found(e):
